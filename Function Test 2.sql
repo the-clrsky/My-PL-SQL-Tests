aeb5cb48167e17f81a2ad0ba18fe0d9da4a8a089
the-clrsky <sadmanbishal@gmail.com>
2024-01-19 00:12:19 +0600

    19/01/2024

---
+++ Function Test 2.sql
@@ -0,0 +1,21 @@
+-- Create FUNCTION
+CREATE OR REPLACE FUNCTION test2
+(p_emp_id EMPLOYEES.EMPLOYEE_ID%TYPE) RETURN NUMBER IS v_sal EMPLOYEES.SALARY%TYPE;
+BEGIN
+SELECT salary INTO v_sal FROM employees
+WHERE employee_id = p_emp_id;
+RETURN v_sal;
+END test2;
+/
+
+-- Invoke FUNCTION with SQL
+SELECT test2(102) FROM dual;
+
+-- Invoke FUNCTION with PL/SQL
+DECLARE
+   v_result_salary EMPLOYEES.SALARY%TYPE;
+BEGIN
+   v_result_salary := test2(105);
+   DBMS_OUTPUT.PUT_LINE('Result Salary: ' || v_result_salary);
+END;
+/
