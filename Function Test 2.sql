58ad2fb705bbcab9f80521cf0d9013822a4efbce
the-clrsky <sadmanbishal@gmail.com>
2024-01-19 00:15:02 +0600

    19/01/2024

--- Function Test 2.sql
+++ Function Test 2.sql
@@ -1,10 +1,15 @@
 -- Create FUNCTION
-CREATE OR REPLACE FUNCTION test2
-(p_emp_id EMPLOYEES.EMPLOYEE_ID%TYPE) RETURN NUMBER IS v_sal EMPLOYEES.SALARY%TYPE;
+CREATE OR REPLACE FUNCTION test2 (p_emp_id EMPLOYEES.EMPLOYEE_ID%TYPE)
+    RETURN NUMBER
+IS
+    v_sal   EMPLOYEES.SALARY%TYPE;
 BEGIN
-SELECT salary INTO v_sal FROM employees
-WHERE employee_id = p_emp_id;
-RETURN v_sal;
+    SELECT salary
+      INTO v_sal
+      FROM employees
+     WHERE employee_id = p_emp_id;
+
+    RETURN v_sal;
 END test2;
 /
 
@@ -13,9 +18,9 @@ SELECT test2(102) FROM dual;
 
 -- Invoke FUNCTION with PL/SQL
 DECLARE
-   v_result_salary EMPLOYEES.SALARY%TYPE;
+    v_result_salary   EMPLOYEES.SALARY%TYPE;
 BEGIN
-   v_result_salary := test2(105);
-   DBMS_OUTPUT.PUT_LINE('Result Salary: ' || v_result_salary);
+    v_result_salary := test2 (105);
+    DBMS_OUTPUT.PUT_LINE ('Result Salary: ' || v_result_salary);
 END;
 /
